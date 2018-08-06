cf9c7b14f5f154984dcb498935612a5cda4b7e70
Faizan Mamji <Faizanmamji1@gmail.com>
2018-08-06 11:16:21 +0500

    Extra libraries deleted.

--- src/main/java/DriverMainPackage/MainDriverClass.java
+++ src/main/java/DriverMainPackage/MainDriverClass.java
@@ -8,8 +8,6 @@ import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import POMMarkITplacePages.POMProductDetail;
